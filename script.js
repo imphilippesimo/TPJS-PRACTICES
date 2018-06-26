887e0677c67f64fb9ecbc2a64ff008aa62d9e5ab
imphilippesimo <philippechampion58@gmail.com>
2018-06-26 22:31:04 +0200

    btt

--- script.js
+++ script.js
@@ -10,18 +10,33 @@
 //le bouton de retour en haut de page
 var btt = document.getElementById("back-to-top"),
 
-//le document
+    //le document
     body = document.body,
-//la racine du document
+    //la racine du document
     docElem = document.documentElement,
-//la limite (100 par defaut)
-    offset = 100;
+    //la limite (100 par defaut)
+    offset = 100,
+    scrollPos, docHeight;
 
+//il faut que l'on sache si l'on est sous firefox car la recuperation de la position courante du curseur différente sur firefox   
+isFirefox = navigator.userAgent.toLowerCase().lastIndexOf("firefox") > -1;
 
 //1)Calcul de la hauteur du document
 docHeight = Math.max(body.scrollHeight, body.offsetHeight, docElem.clientHeight, docElem.scrollHeight, docElem.offsetHeight);
 
+
 //2)offset = ?
+if (docHeight != undefined) {
+    offset = docHeight / 4;
+}
+
 
 //3)Astuce: recuperer la position actuelle du curseur: body.scrollTop || docElem.scrollTop
+window.addEventListener('scroll', function (event) {
+    scrollPos = body.scrollTop || docElem.scrollTop;
+
+
+    btt.className = (scrollPos > offset) ? "visible" : "";
+
+});
 
